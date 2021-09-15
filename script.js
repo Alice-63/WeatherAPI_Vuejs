8fe16aa4210b450c7b94dd386e5ebe8edb7b0061
Alice-63 <alicetindag84@gmail.com>
2021-09-15 23:07:06 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+
+
+const app=new Vue({
+    el:"#app",
+    data:
+    {
+        url:"https://api.openweathermap.org/data/2.5/",
+        key:"1dbd317b286df0d0c3dd61154a5afb6a",
+        searchBar:"",
+        city:"",
+        temp:"",
+        desc:"",
+        minmax:""
+        
+    },
+    methods:
+    {
+        onEnter:function(){
+
+            let query=this.url+"weather?q="+this.searchBar+"&appid="+this.key+"&units=metric&lang=en"
+            fetch(query).then(weather=>{
+                return weather.json()
+            })
+            .then(this.displayResult)
+        },
+        displayResult:function(result){
+            this.city=result.name+","+result.sys.country;
+            this.temp=Math.round(result.main.temp)+"°C";
+            this.desc=result.weather[0].description;
+            this.minmax=Math.round(result.main.temp_max)+"°C / "+Math.round(result.main.temp_min)+"°C"
+            
+        }
+    }
+})
